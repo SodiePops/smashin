9d29ec937d5ca884d0d7977323929b4bd4253cea
Ian J Sikes <ianjsikes@gmail.com>
2017-04-22 21:14:27 -0700

    Update main with example game

---
+++ src/PlayerController.ts
@@ -0,0 +1,13 @@
+import * as tudi from 'tudi'
+
+export default class PlayerController extends tudi.Component {
+  name: string = 'playerController'
+
+  setup (): void {
+    // Do nothing
+  }
+
+  update (): void {
+    // Do something
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,32 @@
 /**
  * Entry point.
  */
-import { Game } from 'tudi'
+import * as tudi from 'tudi'
+import PlayerController from './PlayerController'
 
-const game: Game = new Game(200, 200)
-game.start()
+const player: tudi.Entity = new tudi.Entity(
+  {pos: {x: 100, y: 150}},
+  [
+    new tudi.Components.SpriteComponent('images/player.png'),
+    new PlayerController(),
+  ],
+  [],
+)
+
+const scene: tudi.Scene = new tudi.Scene(
+  ['images/player.png', 'images/floor.png', 'images/enemy.png'],
+  [
+    player,
+    new tudi.Entity(
+      {pos: {x: 20, y: 30}},
+      [new tudi.Components.SpriteComponent('images/floor.png')],
+      []),
+    new tudi.Entity(
+      {pos: {x: 5, y: 10}},
+      [new tudi.Components.SpriteComponent('images/enemy.png')],
+      []),
+  ],
+)
+
+const game: tudi.Game = new tudi.Game(200, 200)
+game.start(scene)
